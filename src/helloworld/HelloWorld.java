11442cd2b5090a55343d17f86124588840ee716c
asus <asus@windows10.microdone.cn>
2018-04-12 21:09:30 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
 		System.out.println("Hello World Hello HJT");
 		System.out.println("Ëæ±ã¸Ä¸Ä£¡");
 		System.err.println("12313");
+		System.out.println("ahhahahahahhahha");
 	}
 }
