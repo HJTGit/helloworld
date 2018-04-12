988c523829a71578684576cf27de32cbd2bcf12f
Administrator <Administrator@2013-20160118TN>
2018-04-12 11:59:22 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World Hello HJT");
 		System.out.println("Ëæ±ã¸Ä¸Ä£¡");
+		System.err.println("12313");
 	}
 }
